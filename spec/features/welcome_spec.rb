6773c82847964b393c0042ccd12b563226aa0b3d
Leah Miller <lkmiller.87@gmail.com>
2019-01-02 20:40:01 -0700

    Resolves merge conflict

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -3,10 +3,6 @@ require 'rails_helper'
 RSpec.describe 'Welcome page', type: :feature do
   it 'displays a simple welcome message' do
     visit root_path
-<<<<<<< HEAD
-    expect(page).to have_content('Welcome to our ficticious')
-=======
     expect(page).to have_content('Welcome to our ficticious "Little Shop of Orders" e-commerce platform.')
->>>>>>> 208a92a0a34f2ef5e997eb8286bbe71fbd6f06fb
   end
 end
