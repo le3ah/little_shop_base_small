37e521490a74ff7b59959b5b8cac91f78522d626
Leah Miller <lkmiller.87@gmail.com>
2019-01-04 11:55:29 -0700

    Adds create, index, and new methods

---
+++ app/controllers/dashboard/discounts_controller.rb
@@ -0,0 +1,20 @@
+class Dashboard::DiscountsController < Dashboard::BaseController
+  def index
+    @discounts = current_user.discounts
+  end
+
+  def new
+    @discount = Discount.new
+  end
+
+  def create
+    @merchant = current_user
+    @discount = @merchant.discounts.create(discount_params)
+  end
+
+  private
+
+  def discount_params
+    params.require(:discount).permit(:discount_type, :discount_amount, :quantity)
+  end
+end
