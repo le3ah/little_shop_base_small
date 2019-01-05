3e82b2e1034b784942ff8e40230d8a1571e651e5
Leah Miller <lkmiller.87@gmail.com>
2019-01-05 14:58:27 -0700

    Adds passing sad path for update

--- spec/features/merchant/merchant_sets_discounts_based_on_cart_spec.rb
+++ spec/features/merchant/merchant_sets_discounts_based_on_cart_spec.rb
@@ -106,6 +106,26 @@ describe  'as a merchant' do
         expect(page).to have_content("Item Quantity: 10")
       end
     end
+    it "should not update if full information is not completed" do
+      merchant_1 = create(:merchant)
+      discount_1 = merchant_1.discounts.create(discount_type: "percentage", discount_amount: "10", quantity: "5")
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(merchant_1)
+
+      visit dashboard_discounts_path
+
+      within "#discount-#{discount_1.id}" do
+        click_link "Edit Discount #{discount_1.id}"
+      end
+      expect(current_path).to eq(edit_dashboard_discount_path(discount_1))
+
+      fill_in :discount_discount_amount, with: 25
+      fill_in :discount_quantity, with: 0
+
+      click_on 'Update Discount'
+
+      expect(current_path).to eq(dashboard_discount_path(discount_1))
+      expect(page).to have_content('Discount update failed')
+    end
   end
   # context "I see the user's cart at a certain quantity" do
   #   it "should apply bulk discount based on cart quantity to pending orders" do
