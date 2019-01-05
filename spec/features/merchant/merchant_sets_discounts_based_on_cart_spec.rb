d00b1c62fb3dce71567e326119346d39e58282ef
Leah Miller <lkmiller.87@gmail.com>
2019-01-04 18:13:10 -0700

    Adds passing test for bulk discount percentage creation

--- spec/features/merchant/merchant_sets_discounts_based_on_cart_spec.rb
+++ spec/features/merchant/merchant_sets_discounts_based_on_cart_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe  'as a merchant' do
   context 'I visit my dashboard page' do
-    it "should click a link to create a bulk discount" do
+    it "should click a link to create a bulk discount - percentage" do
       merchant_1 = create(:merchant)
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(merchant_1)
 
@@ -32,10 +32,32 @@ describe  'as a merchant' do
         expect(page).to have_content("Discount Type: percentage")
         expect(page).to have_content("Discount Amount: 10%")
         expect(page).to have_content("Item Quantity: 5")
-
       end
     end
+    it "should create a bulk discount - dollar" do
+      merchant_1 = create(:merchant)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(merchant_1)
 
+      visit dashboard_discounts_path
+
+      click_link("Create Bulk Discount")
+      expect(current_path).to eq(new_dashboard_discount_path)
+
+      discount_type = "dollar"
+
+      fill_in :discount_discount_type, with: discount_type
+      fill_in :discount_discount_amount, with: 5
+      fill_in :discount_quantity, with: 50
+      click_on 'Create Discount'
+      last_discount = Discount.last
+      expect(current_path).to eq(dashboard_discounts_path)
+
+      within "#discount-#{last_discount.id}" do
+        expect(page).to have_content("Discount Type: dollar")
+        expect(page).to have_content("Discount Amount: $5")
+        expect(page).to have_content("Dollar Limit: $50")
+      end
+    end
   end
   # context "I see the user's cart at a certain quantity" do
   #   it "should apply bulk discount based on cart quantity to pending orders" do
